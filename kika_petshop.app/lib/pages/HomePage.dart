1e16d68e0735dc9c3580ee2feccc50782fa6efb7
Lucas Dias <lucasdasilvadias0610@gmail.com>
2024-03-17 16:15:53 -0300

    devagar ta indo

--- kika_petshop.app/lib/pages/HomePage.dart
+++ kika_petshop.app/lib/pages/HomePage.dart
@@ -1,21 +1,73 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(119, 118, 117, 1),
-      appBar: AppBar(
-        backgroundColor: Colors.deepOrange,
-        title: Text(
-          "Kika Petshop",
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text(
+            "Kika Petshop",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              // Lógica para voltar para a tela anterior
+            },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.calendar_today),
+              onPressed: () {
+                // Lógica para abrir o agendamento
+              },
+            ),
+          ],
         ),
-        centerTitle: true,
-        leading: Container(decoration: BoxDecoration(color: Colors.deepOrange)),
+        body: Center(
+          child: Text('Conteúdo da página'),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.deepOrange,
+          shape: CircularNotchedRectangle(),
+          notchMargin: 8,
+          clipBehavior: Clip.antiAlias,
+          child: SizedBox(
+            height: kBottomNavigationBarHeight,
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.black,
+              selectedItemColor: Colors.amber,
+              unselectedItemColor: Colors.white,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  label: 'Search',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.notifications),
+                  label: 'Notifications',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle),
+                  label: 'Profile',
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
